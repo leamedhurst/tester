26ba88120fb42f8b1a78888bdc6121ee270db37c
Lea Medhurst <lea.medhurst@revelian.com>
2018-04-25 09:15:18 +1000

    first

---
+++ app/routes/movies.js
@@ -0,0 +1,39 @@
+import Ember from 'ember';
+
+let rentals = [{
+  id: 'grand-old-mansion',
+  title: 'Grand Old Mansion',
+  owner: 'Veruca Salt',
+  city: 'San Francisco',
+  type: 'Estate',
+  bedrooms: 15,
+  image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+}, {
+  id: 'urban-living',
+  title: 'Urban Living',
+  owner: 'Mike TV',
+  city: 'Seattle',
+  type: 'Condo',
+  bedrooms: 1,
+  image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
+}, {
+  id: 'downtown-charm',
+  title: 'Downtown Charm',
+  owner: 'Violet Beauregarde',
+  city: 'Portland',
+  type: 'Apartment',
+  bedrooms: 3,
+  image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
+}];
+
+export default Ember.Route.extend({
+	model () {
+    console.log("here" + Ember.$.getJSON('https://graph.facebook.com/?id=http://stackoverflow.com'));
+    var matchdayStats = DS.PromiseObject.create({
+  promise: Em.$.getJSON('https://graph.facebook.com/?id=http://stackoverflow.com')
+});
+console.log(matchdayStats);
+		return Ember.$.getJSON('https://graph.facebook.com/?id=http://stackoverflow.com');
+    //return rentals;
+	}
+});
